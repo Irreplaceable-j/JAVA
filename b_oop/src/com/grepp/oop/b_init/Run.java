ca519ffa59742ea3df1e4364bcceeedc12a17622
Kimoon J <jkm3093@naver.com>
2025-04-25 22:35:41 +0900

    초기화

---
+++ b_oop/src/com/grepp/oop/b_init/Run.java
@@ -0,0 +1,11 @@
+package com.grepp.oop.b_init;
+
+public class Run {
+
+    public static void main(String[] args) {
+        A_init init = new A_init("정기문");
+
+        //A_init.loadClass();
+
+    }
+}
