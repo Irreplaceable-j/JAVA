225fb1b0957c173a3d8e31f3235a87f9c8f986f9
Kimoon J <jkm3093@naver.com>
2025-04-26 23:34:04 +0900

    자료 구조 stack

---
+++ d_library/src/com/grepp/library/c_collection/e_stack/_Stack.java
@@ -0,0 +1,44 @@
+package com.grepp.library.c_collection.e_stack;
+
+import com.grepp.library.c_collection.z_domain.Node;
+import java.util.Iterator;
+
+public class _Stack<E> implements Iterable<E>{
+    private Node<E> top;
+    private int size;
+
+    public void puch(E e){
+        Node<E> node = new Node<>(e);
+        if(top != null){
+            node.setNext(top);         //////////
+        }
+
+        top = node;
+        size++;
+    }
+
+    public E pop(){
+        if(top == null) return null;
+        E data = top.data();
+        top = top.next();
+        size--;
+        return data;
+    }
+    public E peek(){
+        if(top == null) return null;
+        return top.data();
+    }
+    public boolean empty(){
+        return size == 0;
+    }
+    public int size(){
+        return size;
+    }
+
+
+    // LIFO를 지켜서 요소를 반환하는 Iterator
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+}
