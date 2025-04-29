1f18a7fbd4b14ceb35d06f67cc2beb21378929fa
Kimoon J <jkm3093@naver.com>
2025-04-29 20:08:34 +0900

    할인, 가격지불, 구입 기능들 추가

---
+++ df_coffeemanager/src/com/grepp/coffeemanager/domain/order/OrderStatus.java
@@ -0,0 +1,27 @@
+package com.grepp.coffeemanager.domain.order;
+
+// fixme : enum으로 리펙토링
+public enum OrderStatus {
+
+      OK(0, "주문 생성 성공"),
+      FAIL_SOLD_OUT(1, "재고 부족으로 주문을 취소합니다."),
+      FAIL_SEASON(2, "비시즌 입니다.");
+
+    private int code;
+    private String desc;
+
+    private OrderStatus(int code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+    public boolean isOk(){
+        return code == 0;
+    }
+
+    public String desc(){
+        return desc;
+    }
+
+
+
+}
---
+++ df_coffeemanager/src/com/grepp/coffeemanager/domain/sale/Sale.java
@@ -0,0 +1,33 @@
+package com.grepp.coffeemanager.domain.sale;
+
+import com.grepp.coffeemanager.domain.multilingual.payment.ChinaPaymentTranslator;
+import com.grepp.coffeemanager.domain.multilingual.payment.Language;
+import com.grepp.coffeemanager.domain.multilingual.payment.Multilingual;
+import com.grepp.coffeemanager.domain.multilingual.payment.PaymentTranslator;
+import com.grepp.coffeemanager.domain.multilingual.payment.SpainPaymentTranslator;
+import com.grepp.coffeemanager.domain.multilingual.payment.UsaPaymentTranslator;
+import com.grepp.coffeemanager.domain.order.Order;
+import com.grepp.coffeemanager.domain.payment.Payment;
+
+public class Sale {
+
+    public PaymentTranslator takeOrder(Order order) {
+
+        order.proceed();
+        Payment payment = new Payment(order);
+        payment.proceed();
+
+        return generatePaymentTranslator(payment);
+        //return new SpainPaymentTranslator(new UsaPaymentTranslator(new ChinaPaymentTranslator(payment)));
+        //
+    }
+
+    private PaymentTranslator generatePaymentTranslator(Payment payment) {
+        PaymentTranslator target = payment;
+
+        for(Language lang : Multilingual.languages()){
+            target = Language.createPaymentTranslator(lang, target);
+        }
+        return target;
+    }
+}
